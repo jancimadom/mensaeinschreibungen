81b945ce31d4af56fc0ea46e6ba4fa8693848b80
Jan Cimadom <jan.cimadom@sspbruneck1.it>
2026-04-10 08:09:14 +0200

    fix(mail): set secure true for port 465 to fix google smtp

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: parseInt(process.env.SMTP_PORT || '587'),
-  secure: process.env.SMTP_SECURE === 'true',
+  secure: process.env.SMTP_PORT === '465' || process.env.SMTP_SECURE === 'true',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS || process.env.SMTP_PASSWORD,
